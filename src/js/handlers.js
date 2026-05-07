ecf83563530e912a58b0134f6fd2c532bb976d03
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-05-07 10:04:28 +0200

    fix: scroll-locker

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,5 +1,11 @@
 import { refs } from "./refs";
 
-export const openBurger = () => refs.burger.classList.add("is-open");
+export const openBurger = () => {
+  refs.burger.classList.add("is-open");
+  refs.body.classList.add("scroll-lock");
+};
 
-export const closeBurger = () => refs.burger.classList.remove("is-open");
+export const closeBurger = () => {
+  refs.burger.classList.remove("is-open");
+  refs.body.classList.remove("scroll-lock");
+};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
 import { closeBurger, openBurger } from "./js/handlers";
 import { refs } from "./js/refs";
 
-new Accordion('.faq-list', {
-  elementClass: 'faq-list-item',
-  triggerClass: 'faq-acordion-btn',
-  panelClass: 'faq-accordion',
+new Accordion(".faq-list", {
+  elementClass: "faq-list-item",
+  triggerClass: "faq-acordion-btn",
+  panelClass: "faq-accordion",
 });
 
 // header
@@ -15,3 +15,5 @@ refs.burgerBtn.addEventListener("click", openBurger);
 refs.burgerBtnClose.addEventListener("click", closeBurger);
 refs.burgerMenuLink.addEventListener("click", closeBurger);
 refs.burgerMenuBtn.addEventListener("click", closeBurger);
+
+console.log();
