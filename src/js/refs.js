4ea5dbe36ca1595cc0ecfc0159ea2bd82b0dfc06
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-05-08 22:10:45 +0300

    начебто зробила все, я не впевнена щодо кольору при ховері і чи має кількість булетів відповідати як в макеті, хоча це не логічно, буду чекати фідбек

--- src/js/refs.js
+++ src/js/refs.js
@@ -5,4 +5,6 @@ export const refs = {
   burgerBtnClose: document.querySelector(".btn-close"),
   burgerMenuLink: document.querySelector(".menu-link"),
   burgerMenuBtn: document.querySelector(".button-menu"),
+  wrapper: document.querySelector(".about-us-list"),
+  items: document.querySelectorAll(".about-us-item"),
 };
